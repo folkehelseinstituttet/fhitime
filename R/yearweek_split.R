215b7f4aaea0969a81d5f5cf86037fc955565140
chizhang <andreachizhang@yahoo.com>
2021-09-15 09:26:35 +0200

    year change name to isoyear

--- R/yearweek_split.R
+++ R/yearweek_split.R
@@ -1,21 +1,21 @@
-#' isoyearweek_to_year_n
+#' isoyearweek_to_isoyear_n
 #' isoyearweek to year (numeric)
 #' This function breaks the string connected with '-' into year/week
 #' @param yrwk Year-week, e.g. "2020-19" for 19th week in 2020
 #' @export
-isoyearweek_to_year_n <- function(yrwk){
+isoyearweek_to_isoyear_n <- function(yrwk){
   year_n <- stringr::str_split(yrwk, pattern = '-') %>%
     purrr::map_chr(., function(x){x[1]}) %>% as.numeric()
   return(year_n)
 }
 
 
-#' isoyearweek_to_year_c
+#' isoyearweek_to_isoyear_c
 #' isoyearweek to year (character)
 #' This function breaks the string connected with '-' into year/week
 #' @param yrwk Year-week, e.g. "2020-19" for 19th week in 2020
 #' @export
-isoyearweek_to_year_c <- function(yrwk){
+isoyearweek_to_isoyear_c <- function(yrwk){
   year_c <- stringr::str_split(yrwk, pattern = '-') %>%
     purrr::map_chr(., function(x){x[1]})
   return(year_c)
